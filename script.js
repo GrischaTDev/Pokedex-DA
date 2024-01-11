492d700e358986b540e4f74811e4ecb3cbe56fbc
GrischaTDev <grischa.taenzer@gmail.com>
2024-01-11 23:00:47 +0100

    Mobile optimierung und Suchfunktion

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 let allPokemon;
 let currentPokemon;
+let currentSearchPokemon;
 let currentPokemonId = 0;
 let currentPokemonPopup;
 let typeCollor = ['green', 'poison']
 
 async function loadPokemon() {
-    let url = 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=10';
+    let url = 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=50';
     let response = await fetch(url);
     allPokemon = await response.json();
-
-    console.log('Loaded pokemon list', allPokemon); // Ausgabe der API
+    console.log('Loaded all pokemon', allPokemon); // Ausgabe der API
 
     renderAllPokemon();
 }
@@ -34,12 +34,14 @@ async function renderAllPokemon() {
             </div>
             <div class="pokemonId">#${number}</div>
         </div>
-            <img src="img/pokemon/${i + 1}.png" alt="">
+            <img src="img/pokemon/${number}.png" alt="">
             <div>${name}</div>      
         </div>
         `;
     }
-    console.log('Pokemon card details', currentPokemon); // Ausgabe der API  
+    console.log('Pokemon card details', currentPokemon); // Ausgabe der API 
+    document.getElementById('loadingScreen').classList.add('d-none');
+    document.getElementById('body').classList.remove('hide-scrollbar'); 
 }
 
 function typeTemplate(pokemon) {        
@@ -70,6 +72,7 @@ async function pokemonPopup(i) {
 
     document.getElementById('pokemon-stats').innerHTML = /* html */ `
     <div class="pokemon-popup" onclick="closePopup()">
+        <div class="closeMobile" onclick="closePopup()">Close</div>
         <div id="pokemon-popup-card${i}" class="pokemon-popup-card" onclick="notClose(event)">  
         <div class="type-card">
                 <div class="types-content">
@@ -78,14 +81,14 @@ async function pokemonPopup(i) {
                 <div class="pokemonId">#${number}</div>   
             </div>
             <div>${name}</div>     
-            <img src="img/pokemon/${i + 1}.png" alt="">
+            <img src="img/pokemon/${number}.png" alt="">
             <div class="pokemon-info-card">
                 <menu>
                     <div class="pokemon-left" onclick="pokemonPopup(currentPokemonId -1)"><img src="./img/navigate-left.svg"></div>
                     <div class="menu">
                         <div class="menu-start" href="" id="test" onclick="loadAbout()">About</div>
-                        <div class="menu-middle" href="" onclick="loadBaseStats()">Base Stats</div>
-                        <div class="menu-end" onclick="loadEvolution(i)">Evolution</div>
+                        <div class="menu-end" href="" onclick="loadBaseStats()">Base Stats</div>
+                        <!-- <div class="menu-end" onclick="loadEvolution(i)">Evolution</div> -->
                     </div>
                     <div class="pokemon-right" onclick="pokemonPopup(currentPokemonId +1)"><img src="./img/navigate.svg"></div>
                 </menu>
@@ -146,37 +149,59 @@ function loadBaseStats() {
     }
 }
 
-async function loadEvolution(i) {
-    // let url = `https://pokeapi.co/api/v2/evolution-chain/${i}`;
-    let speciesURL = currentPokemonPopup['species']['url'];
-    let response = await fetch(speciesURL);
-    let evolve = await response.json();
+// Aktuell keine funktion!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// async function loadEvolution(i) {
+//     // let url = `https://pokeapi.co/api/v2/evolution-chain/${i}`;
+//     let speciesURL = currentPokemonPopup['species']['url'];
+//     let response = await fetch(speciesURL);
+//     let evolve = await response.json();
 
-    let url2 = evolve['evolution_chain']['url'];
-    let response2 = await fetch(url2);
-    let evolve2 = await response2.json();
+//     let url2 = evolve['evolution_chain']['url'];
+//     let response2 = await fetch(url2);
+//     let evolve2 = await response2.json();
     
 
-    console.log('Loaded pokemon evolve', evolve); // Ausgabe der API
-    console.log('Loaded pokemon evolve', evolve2['chain']['evolves_to']); // Ausgabe der API
-}
+//     console.log('Loaded pokemon evolve', evolve); // Ausgabe der API
+//     console.log('Loaded pokemon evolve', evolve2['chain']['evolves_to']); // Ausgabe der API
+// }
+
+// // let evolutions = allPokemon[index].species.url.chain.evolves_to.length;
+// // let speciesURL = allPokemon[index].species.url;
+// // let speciesData = fetch(speciesURL)
 
 
+async function searchPokemon() {
+    let search = document.getElementById('inputSearch').value;
+    search = search.toLowerCase();
+    pokemon = allPokemon['results'];
 
+    let renderPokemonList = document.getElementById('pokedex');
+    renderPokemonList.innerHTML = '';
 
-// let evolutions = allPokemon[index].species.url.chain.evolves_to.length;
+    for (i = 0; i < pokemon.length; i++) {
+        let pokemonUrl = pokemon[i]['url'];
+        let response = await fetch(pokemonUrl);
+        currentSearchPokemon = await response.json();
 
+        const number = currentSearchPokemon['id'];
+        const name = currentSearchPokemon['name'];
 
-// let speciesURL = allPokemon[index].species.url;
-// let speciesData = fetch(speciesURL)
+        if(name.toLowerCase().includes(search)) {
 
-
-
-
-
-
-function searchPokemon() {
-    document.getElementById('pokemon-search').classList.remove('d-none');
+            renderPokemonList.innerHTML += /* html */ `
+            <div onclick="pokemonPopup(${i})" class="pokemon-card">
+            <div class="type-card">
+                <div class="types-content">
+                    ${typeTemplate(currentSearchPokemon)}
+                </div>
+                <div class="pokemonId">#${number}</div>
+            </div>
+                <img src="img/pokemon/${number}.png" alt="">
+                <div>${name}</div>      
+            </div>
+            `;
+        }
+    }
 }
 
 function closePopup() {
